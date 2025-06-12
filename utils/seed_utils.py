6ca2bfef67076c25ac4d3718486543082eaf4e96
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-12 03:15:08 +0200

    Update seed_utils.py

--- utils/seed_utils.py
+++ utils/seed_utils.py
@@ -2,13 +2,13 @@
 # Seed before Data splitting, Model initialization, DataLoader workers (set worker_init_fn), Training
 
 def set_seed(seed=42):
-    random.seed(seed) # Python's random
-    np.random.seed(seed)  # NumPy
-    torch.manual_seed(seed) # PyTorch (CPU ops)
-    torch.cuda.manual_seed(seed)  # PyTorch (single GPU)
-    torch.cuda.manual_seed_all(seed)  # PyTorch (multi-GPU)
+    random.seed(seed)                          # Python's random
+    np.random.seed(seed)                       # NumPy
+    torch.manual_seed(seed)                    # PyTorch (CPU ops)
+    torch.cuda.manual_seed(seed)               # PyTorch (single GPU)
+    torch.cuda.manual_seed_all(seed)           # PyTorch (multi-GPU)
 
-    torch.backends.cudnn.deterministic = True # Ensures deterministic behavior for convs
-    torch.backends.cudnn.benchmark = False  # Disables auto-tuner for convs (faster but non-deterministic)
+    torch.backends.cudnn.deterministic = True  # Ensures deterministic behavior for convs
+    torch.backends.cudnn.benchmark = False     # Disables auto-tuner for convs (faster but non-deterministic)
 
 set_seed(42)
