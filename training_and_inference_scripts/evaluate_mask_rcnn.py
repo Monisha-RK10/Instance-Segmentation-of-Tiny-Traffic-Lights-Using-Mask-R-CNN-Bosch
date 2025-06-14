1e8851d90f0452986ab5925cc33d0012bd179d13
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-14 04:57:55 +0200

    Update evaluate_mask_rcnn.py

--- training_and_inference_scripts/evaluate_mask_rcnn.py
+++ training_and_inference_scripts/evaluate_mask_rcnn.py
@@ -92,11 +92,12 @@ for idx in tqdm(range(len(val_dataset))):
             continue
 
         mask = masks[i, 0] > 0.5             
-        # Shape (H, W) of ith instance. [num_instances, 1, H, W]. 1 is channel, not used (Mask R-CNN always predicts 1-channel masks). 
-        # Threshold the soft mask to binary using 0.25 (floating-point probability mask to binary mask),
+        # Shape (H, W) of ith instance: [num_instances, 1, H, W] where 1 is channel. Channel is not used as Mask R-CNN always predicts 1-channel mask. 
+        # Threshold the soft mask to binary using 0.25 (floating-point probability mask to binary mask)
         # Changing 0.5 affects which pixels count as foreground -> affects predicted mask shape -> affects IoU -> affects COCO metrics.
-        rle = mask_utils.encode(np.asfortranarray(mask.astype(np.uint8))) 
+
         # Converting the binary mask (after thresholding) into RLE format for COCO evaluator
+        rle = mask_utils.encode(np.asfortranarray(mask.astype(np.uint8))) 
         rle["counts"] = rle["counts"].decode("utf-8")
 
         coco_predictions.append({
